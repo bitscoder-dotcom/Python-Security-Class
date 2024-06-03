0f1aafc4168ba1f164ad7a1e9458c3c0a416e734
exhibit19 <exhibit4real@gmail.com>
2024-06-03 10:14:28 +0100

    first commit

---
+++ securityclass/main.py
@@ -0,0 +1,8 @@
+# print("Hello security class")
+# print("You are targeting "+ input("What ip would you like to target? "))
+
+firstname = input("Enter your first name: ")
+lastname = input("Enter your last name: ")
+
+print("Your names are: "+ firstname + " " + lastname)
+# print("Your names are: ")
