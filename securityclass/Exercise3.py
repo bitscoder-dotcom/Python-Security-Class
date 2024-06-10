b79b6b96aa3827f85df6e3309d4c20de3735569d
bitscoder <daniel.ijedibia@tm30.net>
2024-06-10 08:58:35 +0100

    added third exercise

---
+++ securityclass/Exercise3.py
@@ -0,0 +1,10 @@
+# Create a program that can take input of a users name
+# Save the name in a variable
+# pass the variable through a function and print "Hello _____"
+
+name = input("What is your name: ")
+
+def greeting():
+    print(f"Hello {name}")
+
+greeting()
