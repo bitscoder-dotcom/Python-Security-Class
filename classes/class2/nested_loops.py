fc7399964a5975d1e8ad0830fae47a1fb3509a6c
bitscoder <daniel.ijedibia@tm30.net>
2024-06-12 17:21:40 +0100

    working with abstractions

--- classes/class2/nested_loops.py
+++ classes/class2/nested_loops.py
@@ -1,6 +1,7 @@
 def main():
     # print_square(5)
-    print_square2(4)
+    # print_square2(4)
+    print_square3(4)
 
 def print_square(size):
     # for each row
@@ -16,5 +17,15 @@ def print_square2(size):
     for i in range(size):
         # using string multiplication instead
         print("[]" * size)
+
+        # OR
+
+def print_square3(size):
+    for i in range(size):
+        # using string multiplication instead
+        print_width(size)
+
+def print_width(width):
+    print("[]" * width)
 
 main()
